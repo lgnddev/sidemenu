ba58822763e1cfbb55511128b3ebdf0c1fed8611
Jorge <jor.saezb@duocuc.cl>
2021-09-06 19:46:33 -0300

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,18 @@ const routes: Routes = [
   },  {
     path: 'login',
     loadChildren: () => import('./usuario/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'perfil-usuario',
+    loadChildren: () => import('./usuario/perfil-usuario/perfil-usuario.module').then( m => m.PerfilUsuarioPageModule)
+  },
+  {
+    path: 'recuperar-contrasena',
+    loadChildren: () => import('./usuario/recuperar-contrasena/recuperar-contrasena.module').then( m => m.RecuperarContrasenaPageModule)
+  },
+  {
+    path: 'inscripcion',
+    loadChildren: () => import('./usuario/inscripcion/inscripcion.module').then( m => m.InscripcionPageModule)
   }
 
 ];
