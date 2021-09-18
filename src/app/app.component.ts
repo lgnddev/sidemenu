4fc72069ddf67ec1ea82fdc63f8ab361848a443f
Alonso <alo.zenteno@duocuc.cl>
2021-09-18 17:51:35 -0400

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +6,6 @@ import {Router, NavigationExtras, ActivatedRoute } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  user: any;
 
   public appPages = [
     { title: 'Home', url: '/folder/Inbox', icon: 'home' },
@@ -15,14 +13,5 @@ export class AppComponent {
     { title: 'Tus Recetas', url: '/folder/Outbox', icon: 'book' },
     { title: 'Creador de Recetas', url: '/nueva-receta', icon: 'pencil'},
     { title: 'Favoritos', url: '/folder/Archived', icon: 'heart' },
-    { title: '', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Tus datos', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor(private activatedRoute: ActivatedRoute, private router: Router, private activeroute: ActivatedRoute) {
-    this.activeroute.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.state) {
-        this.user = this.router.getCurrentNavigation().extras.state.user;
-      }
-    });
-  }
 }
