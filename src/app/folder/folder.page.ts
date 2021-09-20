5541a1f7ce1078f944d925a42ac93dee5453428b
Jorge <jor.saezb@duocuc.cl>
2021-09-19 22:34:31 -0300

    cam

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 import {Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
